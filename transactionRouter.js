457de86324387568aa4e0e4b58dca90f71719ee0
hamzanth <irekpitaanthony10@gmail.com>
2023-10-24 06:49:00 +0100

    made the getting the date of transactions route

--- transactionRouter.js
+++ transactionRouter.js
@@ -10,4 +10,18 @@ router.get("/", async function(req, res, next){
     .catch(error => next(error))
 })
 
+router.post('/get-day', async function(req, res, next){
+    const date = req.body.date
+    console.log(new Date(date))
+    await Prods.find({date: {
+        $gte: new Date(new Date(date).setHours(0, 0, 0)),
+        $lt: new Date(new Date(date).setHours(23, 59, 59))
+    }})
+    .then(prods => {
+        res.status(200).json({message: "Successfully gotten all transactions for this day", transactions: prods})
+        
+    })
+    .catch(error => next(error))
+})
+
 module.exports = router
